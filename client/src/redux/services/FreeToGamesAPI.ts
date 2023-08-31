03684564998ce6608e72d9bedeaec80f04b8235b
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2023-08-31 12:44:17 +0300

    fix: any key type

--- client/src/redux/services/FreeToGamesAPI.ts
+++ client/src/redux/services/FreeToGamesAPI.ts
@@ -1,27 +1,31 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Game } from "../../../../types/game";
 
+interface getGamesListOptions {
+  category: string;
+  platform: string;
+}
+
 export const FTGAPI = createApi({
   reducerPath: "FTGAPI",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
   endpoints: (builder) => ({
-    getGamesList: builder.query<Game[], { category: string; platform: string }>(
-      {
-        query: (options) => {
-          const searchParams = new URLSearchParams();
+    getGamesList: builder.query<Game[], getGamesListOptions>({
+      query: (options) => {
+        const searchParams = new URLSearchParams();
 
-          for (const key in options) {
-            const value = options[key];
+        let key: keyof getGamesListOptions;
+        for (key in options) {
+          const value = options[key];
 
-            if (value === "all") continue;
+          if (value === "all") continue;
 
-            searchParams.set(key, value);
-          }
+          searchParams.set(key, value);
+        }
 
-          return "games?" + searchParams;
-        },
-      }
-    ),
+        return "games?" + searchParams;
+      },
+    }),
   }),
 });
 
