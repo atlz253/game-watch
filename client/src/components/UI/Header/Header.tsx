51ad4e952788852b44e0d4b828d598d807735b60
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2023-08-31 04:21:48 +0300

    feat: header style

--- client/src/components/UI/Header/Header.tsx
+++ client/src/components/UI/Header/Header.tsx
@@ -5,7 +5,9 @@ import styles from "./Header.module.css";
 export function Header() {
   return (
     <header className={styles.header}>
-      <Title>Game watch</Title>
+      <a href="/" className={styles.titleWrapper}>
+        <Title className={styles.title}>Game watch</Title>
+      </a>
     </header>
   );
 }
